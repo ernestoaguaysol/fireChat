a1cd8dcbc6af1e82b49615e4f82e4e81dfc695da
Ernesto <ernestoaguaysol@gmail.com>
2018-01-17 12:43:06 -0300

    angularfire2

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBqPg8BzNb0XWY-kjwpbM26O669iRv8VSc",
+    authDomain: "firechat-7deac.firebaseapp.com",
+    databaseURL: "https://firechat-7deac.firebaseio.com",
+    projectId: "firechat-7deac",
+    storageBucket: "firechat-7deac.appspot.com",
+    messagingSenderId: "217098412846"
+  }
 };
